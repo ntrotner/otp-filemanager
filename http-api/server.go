b1e67088b8f8121a53dbb2df42b712b60a1a2706
Nikita Trotner <nikita@ttnr.me>
2022-05-22 23:07:54 +0200

    added health check and initialization of otp endpoints

--- http-api/server.go
+++ http-api/server.go
@@ -5,10 +5,18 @@ import (
 	"net/http"
 )
 
+// start http server to handle requests that relate to backend actions
 func InitializeHTTPServer(port *string) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		// send client that service is running
+		// could be further extended to check if otp is available
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write([]byte(`{"status": "ok"}`))
 	})
+
+	// initialize otp related endpoints
+	OTPHandler()
 
 	log.Println("Listening on Port", *port)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
