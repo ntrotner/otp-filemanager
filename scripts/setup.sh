98ae13bf769dfe1b9bfcff32f1c87ba9baba1c47
Nikita Trotner <nikita@ttnr.me>
2022-12-28 22:16:31 +0100

    add more code quality checks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,7 @@
 
 cp ./scripts/pre-commit ./.git/hooks/
 cp .env-example .env
+
+go install github.com/gordonklaus/ineffassign@latest
+go install github.com/fzipp/gocyclo/cmd/gocyclo@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
